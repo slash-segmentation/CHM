1257cac5b00d0a731c8cef1733e906911f88abd8
Christopher Churas <churas@gmail.com>
2013-12-18 11:02:40 -0800

    added indentation and included try catch logic

--- algorithm/TrainScript_train.m
+++ algorithm/TrainScript_train.m
@@ -1,52 +1,60 @@
-function TrainScript(trainpath,labelpath,savingpath,Nstage,Nlevel);
+function TrainScript_train(trainpath,labelpath,savingpath,Nstage,Nlevel);
 
-if ~strcmp(trainpath(end),'/') 
-    trainpath = [trainpath '/'];
-end
-
-if ~strcmp(labelpath(end),'/')
-    labelpath = [labelpath '/'];
-end
-
-if ~strcmp(savingpath(end),'/')
-    savingpath = [savingpath '/'];
-end
-
-addpath(genpath('FilterStuff')); %add path to functions required for feature extraction.
-mkdir(savingpath);
-
-% Only for preallocation purpose
-filestr = dir([trainpath '*.png']);
-ntr = length(filestr);
-PixN = zeros(Nlevel+1,1);
-for l = 0:Nlevel
-    for i = 1:ntr
-        temp = MyDownSample(imread([trainpath filestr(i).name]),l);
-        PixN(l+1) = PixN(l+1) + numel(temp);
+  try 
+    if ~strcmp(trainpath(end),'/') 
+        trainpath = [trainpath '/'];
     end
-end
-tempfeat = Filterbank(imread([trainpath filestr(1).name]));
-Nfeat = size(tempfeat,1);
-tempfeat = ConstructNeighborhoodsS(imread([trainpath filestr(1).name]));
-Nfeatcontext = size(tempfeat,1);
 
-param.ntr = ntr;
-param.PixN = PixN;
-param.Nfeat = Nfeat;
-param.Nfeatcontext = Nfeatcontext;
-param.Nlevel = Nlevel;
+    if ~strcmp(labelpath(end),'/')
+        labelpath = [labelpath '/'];
+    end
 
-% Train the CHM
-for s = 1:Nstage
-    param.stage = s;
+    if ~strcmp(savingpath(end),'/')
+        savingpath = [savingpath '/'];
+    end
+
+    addpath(genpath('FilterStuff')); %add path to functions required for feature extraction.
+    mkdir(savingpath);
+
+    % Only for preallocation purpose
+    filestr = dir([trainpath '*.png']);
+    ntr = length(filestr);
+    PixN = zeros(Nlevel+1,1);
     for l = 0:Nlevel
-        param.level = l;
-        param.PixN = PixN(l+1);
-        model = trainCHM(trainpath,labelpath,savingpath,param);
-        if s==Nstage, break; end
+        for i = 1:ntr
+            temp = MyDownSample(imread([trainpath filestr(i).name]),l);
+            PixN(l+1) = PixN(l+1) + numel(temp);
+        end
     end
+    tempfeat = Filterbank(imread([trainpath filestr(1).name]));
+    Nfeat = size(tempfeat,1);
+    tempfeat = ConstructNeighborhoodsS(imread([trainpath filestr(1).name]));
+    Nfeatcontext = size(tempfeat,1);
+
+    param.ntr = ntr;
+    param.PixN = PixN;
+    param.Nfeat = Nfeat;
+    param.Nfeatcontext = Nfeatcontext;
+    param.Nlevel = Nlevel;
+
+    % Train the CHM
+    for s = 1:Nstage
+        param.stage = s;
+        for l = 0:Nlevel
+            param.level = l;
+            param.PixN = PixN(l+1);
+            model = trainCHM(trainpath,labelpath,savingpath,param);
+            if s==Nstage, break; end
+        end
+    end
+
+    save([savingpath 'param.mat'],'param');
+
+  catch err
+        fprintf('Caught Fatal Exception: %s\n',err.getReport());
+        exit(1);
+  end
+
+  exit(0);
+
 end
-
-save([savingpath 'param.mat'],'param');
-
-exit;
