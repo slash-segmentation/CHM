fa645703e658ee61e10f032a786cce8a71871080
coderforlife <jeff@coderforlife.com>
2014-05-05 15:26:06 -0700

    Wrong comment style...

--- algorithm/FileExists.m
+++ algorithm/FileExists.m
@@ -1,4 +1,4 @@
-# Workaround for a bug in MATLAB when in that exists(x, 'file') and exists(x, 'dir') give incorrect and inconsistent results
+% Workaround for a bug in MATLAB when in that exists(x, 'file') and exists(x, 'dir') give incorrect and inconsistent results
 function b = FileExists(x, folder);
 if nargin < 1 || nargin > 2; error('FileExists must have 1 or 2 input arguments'); end
 if nargin < 2; folder = false; end
