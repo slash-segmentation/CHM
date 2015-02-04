7bdbabd1bffe1723365431bfe0e511566d5dc56b
Christopher Churas <churas@gmail.com>
2015-02-04 11:09:53 -0800

    Fixed bug in code that tries to obtain stage from MODEL_.. files by replacing curly braces with parenthesis when accessing array from dir function.

--- wrappers/convert_old_param_to_new.m
+++ wrappers/convert_old_param_to_new.m
@@ -5,10 +5,11 @@ if nargin < 2
     Nstage=0;
     files = dir(fullfile(model_folder, 'MODEL_level0_stage*.mat'));
     for i = 1:length(files)
-        if files{i}.isdir; continue; end;
-        NS = sscanf(files{i}.name, 'MODEL_level0_stage%u.mat');
+        if files(i).isdir; continue; end;
+        NS = sscanf(files(i).name, 'MODEL_level0_stage%u.mat');
         if NS > Nstage; Nstage = NS; end;
     end
+    disp(sprintf('Nstage of %d obtained from filenames',Nstage));
 end
 
 param = load(fullfile(model_folder, 'param'), 'param');
