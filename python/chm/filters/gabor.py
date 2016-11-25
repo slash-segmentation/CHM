c6c3b9b2485c7ae5e4f4e175b55d4100b438d253
Jeffrey Bush <jeff@coderforlife.com>
2016-11-25 18:56:31 -0500

    Added support for FFTW wisdom saving/loading for the Gabor filters which greatly speeds them up on subsequent runs. Also fixed up several comments and switched to a single-convolution-function system that also fixes a bug in the value used to scale FFTW transformations.

--- python/chm/filters/gabor.py
+++ python/chm/filters/gabor.py
@@ -34,20 +34,19 @@ class Gabor(Filter):
       lambdaFact: 2, 2.25, 2.5   (lambaFact = lamba / sigma)
       orient:     pi/6, pi/3, pi/2, 2pi/3, 5pi/6, pi, 7pi/6, 4pi/3, 3pi/2, 5pi/3, 11pi/6, 2pi
       gamma:      1
-    Kernels uses a phase of 0 and pi/2 are computed, and combined using the L2 norm into a single
-    feature, resulting in a total of 180 features.
+    Kernels using a phase of 0 and pi/2 are computed to represent the real and imaginary parts, and
+    they combined using the L2 norm (complex magnitude) into a single feature, resulting in a total
+    of 180 features.
 
     The original MATLAB code used real-space transforms while this one uses FFTs which makes it >10x
     faster (since the large FFT for the image itself can be calculated just once) and is just as
     accurate.
 
-    There are two versions of this function, one using the NumPy FFT functions and the other using
-    FFTW if that library is available. The FFTW version is faster and multi-threaded while the
-    NumPy one cannot be used multi-threaded due to a bug in all NumPy version before v1.9.
+    Internally this can use either the NumPy FFT functions or the FFTW library if it is available.
+    The FFTW version is faster and multi-threaded.
 
-    This uses O(4*im.size) intermediate memory. However, O(3*im.size) of that is permanent across
-    calls for each different image size and number of threads given to this function if FFTW is
-    used.
+    This uses O(4*im.size) intermediate memory. However if FFTW is used then O(3*im.size) of that is
+    permanent across calls for each different image size and number of threads given.
 
     Compatibility notes:
     The original MATLAB code has a serious bug in it that it uses the imfilter function with a uint8
@@ -73,25 +72,32 @@ class Gabor(Filter):
         H, W = im.shape[0]-2*padding, im.shape[1]-2*padding # post-filtering shape
         if out is None: out = empty((len(Gabor.__filters), H, W), dtype=im.dtype)
         IF2 = empty((H, W), dtype=im.dtype)
-        conv1, conv2 = Gabor.__get_convolve(im, nthreads)
+        conv = Gabor.__get_convolve(im, nthreads)
         
         for IF1,(GF1,GF2) in izip(out, Gabor.__filters):
             start = len(GF1) // 2 + padding
 
             # Calculate the real and imaginary parts of the filter
-            copy(IF1, conv1(GF1)[start:H+start, start:W+start], nthreads)
-            copy(IF2, conv2(GF2)[start:H+start, start:W+start], nthreads)
+            copy(IF1, conv(GF1)[start:H+start, start:W+start], nthreads)
+            copy(IF2, conv(GF2)[start:H+start, start:W+start], nthreads)
             
             # Get the magnitude of the complex value
             if compat:
                 round_u8_steps(IF1)
                 round_u8_steps(IF2)
             hypot(IF1, IF2, IF1, nthreads)
+            if not compat:
+                # TODO: normalize data
+                pass
             
         return out
 
 
     ##### 'Convolution' Methods #####
+    # These actually use Fourier transforms. There are two versions of the function: one that uses
+    # Numpy and one that uses FFTW (which must be installed separately and is used if possible). Both
+    # functions take the master image and number of threads and return a funcion that will do the
+    # actual convolution itself between the image and a kernel.
     @staticmethod
     def __get_convolve_numpy(im, nthreads):
         # INTERMEDIATE:
@@ -103,10 +109,11 @@ class Gabor(Filter):
         im_fft_sh = tuple(next_regular(x) for x in im.shape)
         im = rfft2(im, s=im_fft_sh)
         def conv(K):
-            F = rfft2(K, s=im_fft_sh)  
-            F *= im
+            from numpy import multiply
+            F = rfft2(K, s=im_fft_sh)
+            multiply(F, im, out=F)
             return irfft2(F, s=im_fft_sh)
-        return conv, conv
+        return conv
 
     @staticmethod
     def __get_convolve_pyfftw(im, nthreads):
@@ -116,24 +123,15 @@ class Gabor(Filter):
         fft_im, fft_gf, ifft = Gabor.__get_fftw_plans(im.shape, nthreads)
         im = Gabor.__fftw(fft_im, im)
         ifft_out = ifft.output_array
-        # TODO: don't precompute division?
-        N1 = 1 / (ifft_out.shape[0] * ifft_out.shape[0]) # scaling factors
-        N2 = 1 / (ifft_out.shape[1] * ifft_out.shape[1])
-        def conv1(K):
+        N = 1 / (ifft_out.shape[0] * ifft_out.shape[1])
+        def conv(K):
             from numpy import multiply
             F = Gabor.__fftw(fft_gf, K)
             multiply(F, im, out=F)
             ifft.execute()
-            multiply(ifft_out, N1, out=ifft_out)
+            multiply(ifft_out, N, out=ifft_out)
             return ifft_out
-        def conv2(K):
-            from numpy import multiply
-            F = Gabor.__fftw(fft_gf, K)
-            multiply(F, im, out=F)
-            ifft.execute()
-            multiply(ifft_out, N2, out=ifft_out)
-            return ifft_out
-        return conv1, conv2
+        return conv
 
     __get_convolve = (__get_convolve_pyfftw if _have_pyfftw else __get_convolve_numpy)
 
@@ -176,18 +174,39 @@ class Gabor(Filter):
             fft_im = FFTW(A, B, (-2, -1), 'FFTW_FORWARD',  ('FFTW_PATIENT', 'FFTW_DESTROY_INPUT'), nthreads)
             fft_gf = FFTW(A, C, (-2, -1), 'FFTW_FORWARD',  ('FFTW_PATIENT', 'FFTW_DESTROY_INPUT'), nthreads)
             ifft   = FFTW(C, A, (-2, -1), 'FFTW_BACKWARD', ('FFTW_PATIENT', 'FFTW_DESTROY_INPUT'), nthreads)
+            Gabor.__save_fftw_wisdom()
             Gabor.__fftw_plans[ID] = plans = (fft_im, fft_gf, ifft)
         return plans
+
+    @staticmethod
+    def __save_fftw_wisdom():
+        """Exports the current FFTW wisdom to a temporary file for possible reuse if possible."""
+        if not _have_pyfftw: return
+        from pyfftw import export_wisdom
+        with open(Gabor.__fftw_wisdom_file, 'wb') as f: f.write(export_wisdom()[0])
     
+    @staticmethod
+    def __load_fftw_wisdom():
+        """Imports FFTW wisdom from a temporary file if possible."""
+        from os.path import isfile
+        if not _have_pyfftw or not isfile(Gabor.__fftw_wisdom_file): return False
+        from pyfftw import import_wisdom
+        with open(Gabor.__fftw_wisdom_file, 'rb') as f: wisdom = f.read()
+        return import_wisdom((wisdom,'',''))[0]
+  
     __fftw_plans = {}
 
 
     ##### Static Fields #####
     __filters = None
     __padding = None
+    __fftw_wisdom_file = None
 
     @staticmethod
     def __get_filters():
+        """
+        Gets all of the convolution filters to use for the Gabor fitler - a total of 360 filters.
+        """
         # Not separated even when possible because FFT convolutions don't work that way
         # Result takes ~2MB, calculation takes ~30ms
         from numpy import pi, arange
@@ -203,11 +222,18 @@ class Gabor(Filter):
     @staticmethod
     def __get_filter(sigma,theta,lmbda,psi,gamma):
         """
-        sigma: Gaussian envelope
-        theta: orientation
-        lmbda: wavelength
-        psi: phase
-        gamma: aspect ratio
+        Returns a matrix with 2*ceil(3*sigma)+1 rows and columns with the values equal to:
+            exp(-(x'^2+gamma^2*y'^2)/(2*sigma^2))*cos(2*pi*x'/lmbda+psi)
+        Where:
+            x' =  x*cos(theta)+y*sin(theta)
+            y' = -x*sin(theta)+y*cos(theta)
+            x,y = center element of matrix
+        With the constants:
+            sigma - Gaussian envelope
+            theta - orientation
+            lmbda - wavelength
+            psi   - phase
+            gamma - aspect ratio
         """
         from numpy import ceil, ogrid, sin, cos, exp, pi, negative
 
@@ -230,13 +256,21 @@ class Gabor(Filter):
         x_theta /= -2*sigma*sigma
         exp(x_theta, out=x_theta)
         x_theta *= ang
-        if psi != 0: negative(x_theta, x_theta) # TODO: this is a HACK to make the Fourier transform versions work
+        if psi != 0:
+            # TODO: this is a hack to make the Fourier transform versions work
+            # (only effects compat mode)
+            negative(x_theta, x_theta)
         x_theta.flags.writeable = False
         return x_theta
-
+    
     @staticmethod
     def __static_init__():
         Gabor.__filters = Gabor.__get_filters()
         Gabor.__padding = max(len(GF[0]) // 2 for GF in Gabor.__filters)
+        if _have_pyfftw:
+            from os.path import join
+            from tempfile import gettempdir
+            Gabor.__fftw_wisdom_file = join(gettempdir(), 'pychm-pyfftw-wisdom')
+            Gabor.__load_fftw_wisdom()
 
 Gabor.__static_init__()
