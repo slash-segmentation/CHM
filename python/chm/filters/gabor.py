a4daa5b22c2614788cc8d2a9eaf8cfe08dab9aaf
Jeffrey Bush <jeff@coderforlife.com>
2017-02-26 14:19:47 -0500

    Adding scaling / normalization to the Gabor filters.

--- python/chm/filters/gabor.py
+++ python/chm/filters/gabor.py
@@ -1,11 +1,7 @@
 """
 Gabor Filter. Implemented with FFTs.
 
-NOTE: I have the real-space ones in a separate document for reviewing and comparing. They do not
-have 'compat' added, but that would be easy to add.
-
-NOTE: also tested doing complex Fourier transforms and it was slower and more code (some parts were
-less code, but overall more).
+NOTE: Also tested real-space ones and complex FFTs but both were slower.
 
 Jeffrey Bush, 2015-2016, NCMIR, UCSD
 """
@@ -43,22 +39,36 @@ class Gabor(Filter):
     accurate.
 
     Internally this can use either the NumPy FFT functions or the FFTW library if it is available.
-    The FFTW version is faster and multi-threaded.
+    The FFTW version is faster and multi-threaded however it has a very large start-up cost to
+    calculate the necessary 'wisdom' the first time it encouters a specific image size and number of
+    threads combinations (resulting in an extra 5+ minutes of time required for the first call that
+    is not present in any future calls). This information is saved between executions of the program
+    as well in the file /tmp/pychm-pyfftw-wisdom (or similar) and can be copied to other machines
+    with similar hardware.
 
     This uses O(4*im.size) intermediate memory. However if FFTW is used then O(3*im.size) of that is
     permanent across calls for each different image size and number of threads given.
 
-    Compatibility notes:
     The original MATLAB code has a serious bug in it that it uses the imfilter function with a uint8
     input which causes the filter output to be clipped to 0-255 and rounded to an integer. This is a
     major problem as the Gabor filters have negative values (all of which are lost) and can produce
-    results above the input range, along with the losing lots of resolution in the data (from ~16
-    significant digits to ~3). So, when compat is True (the default), the data is put through the
-    same simplification process. When it is False, then much better results are produced.
+    results above the input range, along with losing lots of resolution in the data (from ~16
+    significant digits to ~3). So, when compat is True, the data is put through the same
+    simplification process. When it is False (the default), then much better results are produced.
+    
+    The scale flag causes the output data to be scaled so that each filter roughly goes from 0 to 1.
+    Every feature has a different scaling factor as the range for each is different. No shifting is
+    done since the lowest value of each feature is already 0. It defaults to the opposite of the
+    compat flag. If both compat and scale are True the results will likely be fairly meaningless.
     """
-    def __init__(self, compat=False):
+    # Defaults for Python models created before these flags were added
+    __compat = True
+    __scale = False
+    
+    def __init__(self, compat=False, scale=None):
         super(Gabor, self).__init__(Gabor.__padding, len(Gabor.__filters))
         self.__compat = compat
+        self.__scale = (not compat) if scale is None else scale
 
     def __call__(self, im, out=None, region=None, nthreads=1):
         # INTERMEDIATE: im.shape (for either numpy or pyfttw)
@@ -66,8 +76,7 @@ class Gabor(Filter):
         from numpy import empty
         from ._base import get_image_region, round_u8_steps, hypot, copy
 
-        padding = Gabor.__padding
-        compat = self.__compat
+        padding, compat = Gabor.__padding, self.__compat
         im, region = get_image_region(im, padding, region, nthreads=nthreads)
         H, W = im.shape[0]-2*padding, im.shape[1]-2*padding # post-filtering shape
         if out is None: out = empty((len(Gabor.__filters), H, W), dtype=im.dtype)
@@ -86,10 +95,8 @@ class Gabor(Filter):
                 round_u8_steps(IF1)
                 round_u8_steps(IF2)
             hypot(IF1, IF2, IF1, nthreads)
-            if not compat:
-                # TODO: normalize data
-                pass
-            
+
+        if self.__scale: out *= Gabor.__normalizations
         return out
 
 
@@ -99,7 +106,7 @@ class Gabor(Filter):
     # functions take the master image and number of threads and return a funcion that will do the
     # actual convolution itself between the image and a kernel.
     @staticmethod
-    def __get_convolve_numpy(im, nthreads):
+    def __get_convolve_numpy(im, nthreads): ##pylint: disable=unused-argument
         # INTERMEDIATE:
         #     2*(im.shape[0],(im.shape[1]+1)//2) + (?,?)
         #     2*(im.shape[0],(im.shape[1]+1)//2) + (?,?)    [during call to conv]
@@ -180,7 +187,7 @@ class Gabor(Filter):
 
     @staticmethod
     def __save_fftw_wisdom():
-        """Exports the current FFTW wisdom to a temporary file for possible reuse if possible."""
+        """Exports the current FFTW wisdom to a temporary file for reuse if possible."""
         if not _have_pyfftw: return
         from pyfftw import export_wisdom
         with open(Gabor.__fftw_wisdom_file, 'wb') as f: f.write(export_wisdom()[0])
@@ -199,13 +206,14 @@ class Gabor(Filter):
 
     ##### Static Fields #####
     __filters = None
+    __normalizations = None
     __padding = None
     __fftw_wisdom_file = None
 
     @staticmethod
     def __get_filters():
         """
-        Gets all of the convolution filters to use for the Gabor fitler - a total of 360 filters.
+        Gets all of the convolution kernels to use for the Gabor fitler - a total of 360 filters.
         """
         # Not separated even when possible because FFT convolutions don't work that way
         # Result takes ~2MB, calculation takes ~30ms
@@ -227,7 +235,7 @@ class Gabor(Filter):
         Where:
             x' =  x*cos(theta)+y*sin(theta)
             y' = -x*sin(theta)+y*cos(theta)
-            x,y = center element of matrix
+            x,y = distance from center of kernel
         With the constants:
             sigma - Gaussian envelope
             theta - orientation
@@ -262,10 +270,41 @@ class Gabor(Filter):
             negative(x_theta, x_theta)
         x_theta.flags.writeable = False
         return x_theta
+
+    @staticmethod
+    def __get_normalizations(filters):
+        """
+        Calculates the inverse of the normalization factors for each of the filters. Currently this
+        uses the real filter threshold at 0 and applies both the real and imaginary filters to it
+        finding the complex magnitude for each filter.
+        
+        A better solution may be to figure out the standard deviations caused be each real/imaginary
+        pair and use those.
+        """
+        from numpy import array
+        return 1.0/array([
+                1.99,2.45,3.01,2.02,2.52,3.12,1.86,2.42,3.07,1.98,2.42,2.98,1.97,2.41,2.94,1.81,2.29,2.87,
+                1.99,2.45,3.01,2.02,2.52,3.12,1.86,2.42,3.07,1.98,2.42,2.98,1.97,2.41,2.94,1.81,2.29,2.87,
+                4.69,5.66,6.94,4.93,6.17,7.61,4.96,6.35,7.90,4.51,5.58,6.96,4.53,5.33,6.49,4.43,5.32,6.52,
+                4.69,5.66,6.94,4.93,6.17,7.61,4.96,6.35,7.90,4.51,5.58,6.96,4.53,5.33,6.49,4.43,5.32,6.52,
+                8.63,10.8,13.1,10.1,12.5,14.9,10.6,13.4,16.1,8.59,11.0,13.7,7.81,9.60,11.9,7.86,9.77,12.1,
+                8.63,10.8,13.1,10.1,12.5,14.9,10.6,13.4,16.1,8.59,11.0,13.7,7.81,9.60,11.9,7.86,9.77,12.1,
+                15.1,18.4,21.8,18.2,21.7,25.1,19.8,23.9,27.6,15.8,19.7,23.5,13.1,16.1,19.6,13.4,16.5,19.9,
+                15.1,18.4,21.8,18.2,21.7,25.1,19.8,23.9,27.6,15.8,19.7,23.5,13.1,16.1,19.6,13.4,16.5,19.9,
+                24.1,28.4,32.6,28.9,33.2,37.5,32.5,37.4,42.0,26.3,31.5,36.4,20.8,25.1,29.8,21.1,25.2,29.8,
+                24.1,28.4,32.6,28.9,33.2,37.5,32.5,37.4,42.0,26.3,31.5,36.4,20.8,25.1,29.8,21.1,25.2,29.8])
+        #from numpy import empty, sqrt, divide
+        #norms = empty(len(filters))
+        #for i,(GF1,GF2) in enumerate(filters):
+        #    x = (GF1>=0).astype(float) # TODO: make this better
+        #    IF1,IF2 = (x*GF1).sum(),(x*GF2).sum() # no need to actually do convolution
+        #    norms[i] = IF1*IF1+IF2*IF2
+        #return divide(1, sqrt(norms, norms), norms) # 1/sqrt(...)
     
     @staticmethod
     def __static_init__():
         Gabor.__filters = Gabor.__get_filters()
+        Gabor.__normalizations = Gabor.__get_normalizations(Gabor.__filters)[:,None,None]
         Gabor.__padding = max(len(GF[0]) // 2 for GF in Gabor.__filters)
         if _have_pyfftw:
             from os.path import join
