7bba47379ee618eae9c34a39426fd7f1afd880e7
Jeffrey Bush <jeff@coderforlife.com>
2017-03-18 17:29:13 -0400

    Add functions and a command line program to calculate the performance of the training by comparing the predicted images with ground truth data and reporting the accuracy, F-value, and G-mean.

---
+++ python/chm/perf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+"""
+Calculate the accuracy/performance of a set of images compared to the ground truth data.
+
+Jeffrey Bush, 2017, NCMIR, UCSD
+"""
+
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import print_function
+
+def main():
+    from .utils import calc_confusion_matrix, calc_accuracy, calc_fvalue, calc_gmean
+    from pysegtools.images.io import FileImageStack
+    from pysegtools.images.filters.threshold import ThresholdImageStack
+
+    from sys import exit, argv
+    if len(argv) != 3:
+        print("Requires 2 arguments: a stack of predicted images and a stack of ground-truth images")
+        exit(1)
+    
+    predicted = ThresholdImageStack(FileImageStack.open_cmd(argv[1]), 'auto-stack')
+    ground_truth = ThresholdImageStack(FileImageStack.open_cmd(argv[2]), 1)
+    
+    confusion_matrix = calc_confusion_matrix(predicted, ground_truth)
+    print("Accuracy = %f"%calc_accuracy(*confusion_matrix))
+    print("F-value  = %f"%calc_fvalue(*confusion_matrix))
+    print("G-mean   = %f"%calc_gmean(*confusion_matrix))
+
+if __name__ == "__main__": main()
