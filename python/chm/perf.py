4e94519e9535ffe44eefa184658e88915168a5dc
Jeffrey Bush <jeff@coderforlife.com>
2017-08-20 10:57:50 -0400

    Improvements to the perf command.

--- python/chm/perf.py
+++ python/chm/perf.py
@@ -20,10 +20,18 @@ def main():
         print("Requires 2 arguments: a stack of predicted images and a stack of ground-truth images")
         sys.exit(1)
     
-    predicted = ThresholdImageStack(FileImageStack.open_cmd(sys.argv[1]), 'auto-stack')
-    ground_truth = ThresholdImageStack(FileImageStack.open_cmd(sys.argv[2]), 1)
+    # Open the image stacks
+    predicted = FileImageStack.open_cmd(sys.argv[1])
+    ground_truth = FileImageStack.open_cmd(sys.argv[2])
     
+    # Wrap them in a thresholding image stack if they are not already boolean
+    if not predicted.is_dtype_homogeneous or predicted.dtype != bool:
+        predicted = ThresholdImageStack(predicted, 'auto-stack')
+    if not ground_truth.is_dtype_homogeneous or ground_truth.dtype != bool:
+        ground_truth = ThresholdImageStack(ground_truth, 1)
+
     confusion_matrix = calc_confusion_matrix(predicted, ground_truth)
+    print("TP = %d, TN = %d, FP = %d, FN = %d"%confusion_matrix)
     print("Accuracy = %f"%calc_accuracy(*confusion_matrix))
     print("F-value  = %f"%calc_fvalue(*confusion_matrix))
     print("G-mean   = %f"%calc_gmean(*confusion_matrix))
